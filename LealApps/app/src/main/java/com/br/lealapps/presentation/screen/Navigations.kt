f7669a567d2b839ac1128d5c37897b0a73e3972a
Vinicius <viniciusmach07@gmail.com>
2023-12-27 16:40:49 -0300

    Remove unused import

--- LealApps/app/src/main/java/com/br/lealapps/presentation/screen/Navigations.kt
+++ LealApps/app/src/main/java/com/br/lealapps/presentation/screen/Navigations.kt
@@ -1,7 +1,6 @@
 package com.br.lealapps.presentation.screen
 
 import androidx.compose.runtime.Composable
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
