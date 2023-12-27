35126850e49fcd86d9a6be24c5ba513e43b93e8c
Vinicius <viniciusmach07@gmail.com>
2023-12-27 14:10:35 -0300

    Create screen to add a new training

--- LealApps/app/src/main/java/com/br/lealapps/presentation/screen/AddTrainingScreen.kt
+++ LealApps/app/src/main/java/com/br/lealapps/presentation/screen/AddTrainingScreen.kt
@@ -1,29 +1,51 @@
 package com.br.lealapps.presentation.screen
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Save
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.br.lealapps.data.source.model.Exercicio
+import com.br.lealapps.data.source.model.Treino
 import com.br.lealapps.presentation.screen.common.CommonNavigationBar
 import com.br.lealapps.presentation.viewmodel.HomeViewModel
+import java.util.Date
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AddTrainingScreen(navController: NavController, viewModel: HomeViewModel) {
-    val treinos by viewModel.treinos.collectAsState()
+    val exercicios by viewModel.exercicios.observeAsState(emptyList())
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -31,11 +53,6 @@ fun AddTrainingScreen(navController: NavController, viewModel: HomeViewModel) {
                 navigationIcon = {
                     IconButton(onClick = { navController.popBackStack() }) {
                         Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "Voltar")
-                    }
-                },
-                actions = {
-                    IconButton(onClick = { navController.navigate("addTrainingScreen") }) {
-                        Icon(imageVector = Icons.Default.Add, contentDescription = "Add")
                     }
                 },
                 colors = TopAppBarDefaults.mediumTopAppBarColors(
@@ -47,8 +64,101 @@ fun AddTrainingScreen(navController: NavController, viewModel: HomeViewModel) {
             )
         },
         content = {
-            TreinosList(viewModel = viewModel, navController = navController)
+            AddingTraining(exercicios = exercicios, viewModel = viewModel)
         },
-        bottomBar = { CommonNavigationBar(navController = navController) }
+        bottomBar = { CommonNavigationBar(navController = navController) },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    //viewModel.addTreino(treinoToAdd)
+                    navController.popBackStack()
+                },
+                containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                contentColor = MaterialTheme.colorScheme.primary,
+            ) {
+                Icon(imageVector = Icons.Default.Save, contentDescription = "Salvar")
+            }
+
+        }
     )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddingTraining(exercicios: List<Exercicio>, viewModel: HomeViewModel) {
+    var nome by remember { mutableStateOf("") }
+    var descricao by remember { mutableStateOf("") }
+    var data by remember { mutableStateOf("") }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp, vertical = 72.dp)
+    ) {
+        // Campo de Nome do Treino
+        OutlinedTextField(
+            value = nome,
+            onValueChange = { nome = it },
+            label = { Text("Nome do Treino") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp)
+        )
+
+        // Campo de Descrição do Treino
+        OutlinedTextField(
+            value = descricao,
+            onValueChange = { descricao = it },
+            label = { Text("Descrição do Treino") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp)
+        )
+
+        // Campo de Data do Treino
+        OutlinedTextField(
+            value = data,
+            onValueChange = { data = it },
+            label = { Text("Data do Treino") },
+            trailingIcon = {
+                Icon(
+                    imageVector = Icons.Default.DateRange,
+                    contentDescription = "Data do Treino"
+                )
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp)
+        )
+
+        // Lista de Exercícios com Checkboxes
+        LazyColumn {
+            itemsIndexed(exercicios) { _, exercicio ->
+                val isChecked = !exercicios.contains(exercicio)
+                var checkboxState by remember { mutableStateOf(isChecked) }
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 4.dp)
+                ) {
+                    Checkbox(
+                        checked = checkboxState,
+                        onCheckedChange = {
+                            checkboxState = it
+                        },
+                        modifier = Modifier
+                            .align(Alignment.CenterVertically)
+                            .clickable { checkboxState = !checkboxState }
+                    )
+                    Text(
+                        text = exercicio.nome,
+                        modifier = Modifier
+                            .weight(1f)
+                            .padding(start = 8.dp)
+                            .align(Alignment.CenterVertically)
+                    )
+                }
+            }
+        }
+    }
 }
